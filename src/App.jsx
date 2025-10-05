c5673007977e205ea9c0e82db33899dfc852475c
amaan1114 <amaan.zaidi05@gmail.com>
2025-10-05 07:12:20 +0530

    Bug Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => (
             {/* <Route path="/" element={<Index />} /> */}
             <Route path="/" element={<Home />} />
             <Route 
-              path="/dashboard" 
+              path="//dashboard" 
               element={
                 <ProtectedRoute>
                   <Dashboard />
